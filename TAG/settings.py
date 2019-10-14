1874ec49b0adfa7c6063d741eed3aa79b1b5b1bc
Nico Gonzalez <nicog96@gmail.com>
2019-10-14 14:43:56 -0400

    Add config file for secret keys
    Remove __init__
    
    Update gitignore

--- TAG/settings.py
+++ TAG/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import configparser
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+config = configparser.ConfigParser()
+config.read('config.ini')
+SECRET_KEY = config.get("DJANGO SECRET", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
