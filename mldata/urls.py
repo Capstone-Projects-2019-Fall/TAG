b42a743fa3a8aab818e040b47b1393ef31e0b2fe
Tmarkn <39633888+tmarkn@users.noreply.github.com>
2019-10-24 11:37:37 -0400

    added test page for webapi
    + timer to time algorithm

--- mldata/urls.py
+++ mldata/urls.py
@@ -3,4 +3,6 @@ from mldata import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/index', views.index, name='index'),
+    path('/APItest', views.APItest, name='APItest'),
 ]
--- mldata/views.py
+++ mldata/views.py
@@ -3,26 +3,34 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 # replace sorcery with machine learning program (.py)
 from mldata.sorcery import magic
-
+from django.shortcuts import render
+import time
 
 @csrf_exempt
 def index(request):
     # get request
     if request.method == 'GET':
         noInputResponse = 'Welcome to the our totally working api! \n'
-        noInputResponse += 'To use our API, send a POST request with a JSON file attached as "file" in form data'
+        noInputResponse += 'To use our API, send a POST request with a JSON file attached as "jsonUpload" in form data'
         return HttpResponse(noInputResponse)
     # post request
     elif request.method == 'POST':
+        startTime = time.time()
         # get file
-        xfile = request.FILES['file']
+        xfile = request.FILES['jsonUpload']
         # did get input
         # prepare data
         try:
             data = json.loads(xfile.read())
+            print(data)
         except json.decoder.JSONDecodeError:
             return HttpResponse('Json formatted incorrectely! Please fix then try again!')
         jsonData = json.dumps(data)
         # return data from machine learning algorithm
         output = magic(jsonData)  # replace with machine learning algorithm
+        endTime = time.time()
+        print("Elapsed Time: " + str(endTime-startTime))
         return HttpResponse(output, content_type='application/json')
+
+def APItest(request):
+    return render(request, 'APItest.html')
