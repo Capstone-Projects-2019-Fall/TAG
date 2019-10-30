20949dfdb4f1673a97d3d0d8d4722ced4cc452ea
keithudock <keithudock@gmail.com>
2019-10-29 22:01:44 -0400

    Added search button and corresponding console log

--- static/js/search.js
+++ static/js/search.js
@@ -1,11 +1,18 @@
 // Toggle Regex button to inactive if Plain Text is selected
 $("#plain-text").on("click", function(){
-    $("#regex").attr("class", "btn btn-mdb-color")
+    $("#plain-text").attr("aria-pressed", "true")
+    $("#regex").attr("aria-pressed", "false")
     console.log("Selected plain text search")
-})
+});
 
 // Toggle Plain text button to inactive if Regex is selected
 $("#regex").on("click", function(){
-    $("#plain-text").attr("class", "btn btn-mdb-color")
+    $("#regex").attr("aria-pressed", "true")
+    $("#plain-text").attr("aria-pressed", "false")
     console.log("Selected regex search")
-})
+});
+
+$("#search-button").on("click", function(){
+    var searching = document.getElementById("search-box").value;
+    console.log("Searching for " + searching + " in document");
+});
