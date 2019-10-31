513c56ceed925c53fa1ee0f4ccd1de5c8cfdf6c1
keithudock <keithudock@gmail.com>
2019-10-31 15:05:16 -0400

    Searched text is highlighted

--- static/js/search.js
+++ static/js/search.js
@@ -13,14 +13,32 @@ $("#regex").on("click", function(){
 });
 
 $("#search-button").on("click", function(){
+    // Get the text the user is searching for
     var searching = document.getElementById("search-box").value;
+    var regex = new RegExp(searching, 'g');
     console.log("Searching for " + searching + " in document");
 
+    //Get the contents of the entire document
     var text = document.getElementById("doc-view").value;
+    // Split text into tokens
+    var token = text.split(/\s/);
 
-    if(searching.length > 0 && text.indexOf(searching) > -1) {
-        console.log("Success! " + searching + " found in current document");
-    } else {
-        console.log("Failure... " + searching + " not found in current document");
-    }
+    //Current position in the document
+    var position = 0;
+      
+    token.forEach( function(word){
+        if(word.match(regex)){
+            console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
+            let range = {
+                'startPosition': position,
+                'endPosition': position + word.length
+              };
+            let annotationCreated = tagModel.addAnnotation(range, tagModel.currentCategory);
+            console.log("Highlighted: " + range.startPosition + "-" + range.endPosition);
+            $('#recent').text(annotationCreated.content.trunc(50, true));
+            addToList(annotationCreated.content);
+        }
+        position += word.length+1; // the plus 1 accounts for the space at the end
+    })
+    renderTextareaHighlights();
 });
