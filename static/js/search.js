fb9c323ba2c7c2b6a0b5536e4db385119a5352c2
keithudock <keithudock@gmail.com>
2019-11-01 12:48:12 -0400

    Forgot to add search to delete to search button

--- static/js/search.js
+++ static/js/search.js
@@ -1,33 +1,59 @@
 // Searching using the search button
 $("#search-button").on("click", function(){
     // Get the text the user is searching for
-    var searching = document.getElementById("search-box").value;
-    var regex = new RegExp(searching, 'g');
-    console.log("Searching for " + searching + " in document");
+    if($("#search-type").children("option:selected").val() === "add") {
+        console.log("Searching to highlight");
+        // Get the text the user is searching for
+        var searching = document.getElementById("search-box").value;
+        var regex = new RegExp(searching, 'g');
+        console.log("Searching for " + searching + " in document"); 
 
-    //Get the contents of the entire document
-    var text = document.getElementById("doc-view").value;
-    // Split text into tokens
-    var token = text.split(/\s/);
+        //Get the contents of the entire document
+        var text = document.getElementById("doc-view").value;
+        // Split text into tokens
+        var token = text.split(/\s/);
 
-    //Current position in the document
-    var position = 0;
-      
-    token.forEach( function(word){
-        if(word.match(regex)){
-            console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
-            let range = {
-                'startPosition': position,
-                'endPosition': position + word.length
-              };
-            let annotationCreated = tagModel.addAnnotation(range, tagModel.currentCategory);
-            console.log("Highlighted: " + range.startPosition + "-" + range.endPosition);
-            $('#recent').text(annotationCreated.content.trunc(50, true));
-            addToList(annotationCreated.content);
-        }
-        position += word.length+1; // the plus 1 accounts for the space at the end
-    })
-    renderTextareaHighlights();
+        //Current position in the document
+        var position = 0;
+  
+        token.forEach( function(word){
+            if(word.match(regex)){
+                console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
+                let range = {
+                    'startPosition': position,
+                    'endPosition': position + word.length
+                };
+                let annotationCreated = tagModel.addAnnotation(range, tagModel.currentCategory);
+                console.log("Highlighted: " + range.startPosition + "-" + range.endPosition);
+                $('#recent').text(annotationCreated.content.trunc(50, true));
+                addToList(annotationCreated.content);
+            }
+            position += word.length+1; // the plus 1 accounts for the space at the end
+        })
+        renderTextareaHighlights();
+    } else if ($("#search-type").children("option:selected").val() === "delete") {
+        console.log("Searching to delete.");
+        var searching = document.getElementById("search-box").value;
+        var regex = new RegExp(searching, 'g');
+        console.log("Searching for " + searching + " in document"); 
+
+        //Get the contents of the entire document
+        var text = document.getElementById("doc-view").value;
+        // Split text into tokens
+        var token = text.split(/\s/);
+
+        //Current position in the document
+        var position = 0;
+  
+        token.forEach( function(word){
+            if(word.match(regex)){
+                console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
+                tagModel.removeAnnotation(position)
+            }
+            position += word.length+1; // the plus 1 accounts for the space at the end
+        })
+        renderTextareaHighlights();
+    }
 });
 
 $("#search-box").on("keypress", function(e){
