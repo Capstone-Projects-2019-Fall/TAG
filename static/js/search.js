6045432ddb47155455c92eaf50fffbe2da6cd1a4
Keith <keithudock@gmail.com>
2019-11-03 10:06:15 -0500

    Bug fixes

--- static/js/search.js
+++ static/js/search.js
@@ -5,75 +5,13 @@ $("#search-button").on("click", function(){
         console.log("Searching to highlight");
         // Get the text the user is searching for
         var searching = document.getElementById("search-box").value;
-
-        // Get flags selected from dropdown;
-        var flags = [];
-        $.each($("input[name='flag']:checked"), function(){
-            flags.push($(this).val());
-        });
-        console.log("Flags: " + flags.join(''));
-
-        var regex = new RegExp("\\b" + searching + "\\b", flags.join(''));
-        console.log("Searching for " + searching + " in document"); 
-
-        //Get the contents of the entire document
-        var text = document.getElementById("doc-view").value;
-        // Split text into tokens
-        var token = text.split(/\s/);
-
-        //Current position in the document
-        var position = 0;
-  
-        token.forEach( function(word){
-            if(word.match(regex)){
-                console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
-                let range = {
-                    'startPosition': position,
-                    'endPosition': position + word.length
-                };
-                let annotationCreated = tagModel.addAnnotation(range, tagModel.currentCategory);
-                console.log("Highlighted: " + range.startPosition + "-" + range.endPosition);
-                $('#recent').text(annotationCreated.content.trunc(50, true));
-                addToList(annotationCreated.content);
-            }
-            position += word.length+1; // the plus 1 accounts for the space at the end
-        })
-        renderTextareaHighlights();
-    } else if ($("#search-type").children("option:selected").val() === "delete") {
-        console.log("Searching to delete.");
-        var searching = document.getElementById("search-box").value;
-        var regex = new RegExp("\\b" + searching, 'g');
-        console.log("Searching for " + searching + " in document"); 
-
-        //Get the contents of the entire document
-        var text = document.getElementById("doc-view").value;
-        // Split text into tokens
-        var token = text.split(/\s/);
-
-        //Current position in the document
-        var position = 0;
-  
-        token.forEach( function(word){
-            if(word.match(regex)){
-                console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
-                tagModel.removeAnnotation(position)
-            }
-            position += word.length+1; // the plus 1 accounts for the space at the end
-        })
-        renderTextareaHighlights();
-    }
-});
-
-$("#search-box").on("keypress", function(e){
-    if(e.which === 13){
-        if(!$(this).val()){
+        if (tagModel.currentCategory === null) {
+            alert('Error: Please select a category first');
             return;
-        }
-        if($("#search-type").children("option:selected").val() === "add") {
-            console.log("Searching to highlight");
-            // Get the text the user is searching for
-            var searching = document.getElementById("search-box").value;
-
+        } else if (searching == "" || searching.trim() == ""){
+            alert("Please enter the text you want to highlight");
+            return;
+        } else {
             // Get flags selected from dropdown;
             var flags = [];
             $.each($("input[name='flag']:checked"), function(){
@@ -91,7 +29,7 @@ $("#search-box").on("keypress", function(e){
 
             //Current position in the document
             var position = 0;
-      
+  
             token.forEach( function(word){
                 if(word.match(regex)){
                     console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
@@ -106,11 +44,19 @@ $("#search-box").on("keypress", function(e){
                 }
                 position += word.length+1; // the plus 1 accounts for the space at the end
             })
-            renderTextareaHighlights();
-        } else if ($("#search-type").children("option:selected").val() === "delete") {
-            console.log("Searching to delete.");
-            var searching = document.getElementById("search-box").value;
-            var regex = new RegExp(searching, 'g');
+            renderTextareaHighlights(); 
+            }    
+    } else if ($("#search-type").children("option:selected").val() === "delete") {
+        console.log("Searching to delete.");
+        var searching = document.getElementById("search-box").value;
+        if (tagModel.currentCategory === null) {
+            alert('Error: Please select a category first');
+            return;
+        } else if (searching == "" || searching.trim() == ""){
+            alert("Please enter the text you want to highlight");
+            return;
+        } else {
+            var regex = new RegExp("\\b" + searching, 'g');
             console.log("Searching for " + searching + " in document"); 
 
             //Get the contents of the entire document
@@ -120,7 +66,7 @@ $("#search-box").on("keypress", function(e){
 
             //Current position in the document
             var position = 0;
-      
+  
             token.forEach( function(word){
                 if(word.match(regex)){
                     console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
@@ -129,6 +75,90 @@ $("#search-box").on("keypress", function(e){
                 position += word.length+1; // the plus 1 accounts for the space at the end
             })
             renderTextareaHighlights();
+        }
+    }
+});
+
+$("#search-box").on("keypress", function(e){
+    if(e.which === 13){
+        if(!$(this).val()){
+            return;
+        }
+        if($("#search-type").children("option:selected").val() === "add") {
+            console.log("Searching to highlight");
+            // Get the text the user is searching for
+            var searching = document.getElementById("search-box").value;
+            if (tagModel.currentCategory === null) {
+                alert('Error: Please select a category first');
+                return;
+            } else if (searching == "" || searching.trim() == ""){
+                alert("Please enter the text you want to highlight");
+                return;
+            } else {
+                // Get flags selected from dropdown;
+                var flags = [];
+                $.each($("input[name='flag']:checked"), function(){
+                    flags.push($(this).val());
+                });
+                console.log("Flags: " + flags.join(''));
+    
+                var regex = new RegExp("\\b" + searching + "\\b", flags.join(''));
+                console.log("Searching for " + searching + " in document"); 
+    
+                //Get the contents of the entire document
+                var text = document.getElementById("doc-view").value;
+                // Split text into tokens
+                var token = text.split(/\s/);
+    
+                //Current position in the document
+                var position = 0;
+      
+                token.forEach( function(word){
+                    if(word.match(regex)){
+                        console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
+                        let range = {
+                            'startPosition': position,
+                            'endPosition': position + word.length
+                        };
+                        let annotationCreated = tagModel.addAnnotation(range, tagModel.currentCategory);
+                        console.log("Highlighted: " + range.startPosition + "-" + range.endPosition);
+                        $('#recent').text(annotationCreated.content.trunc(50, true));
+                        addToList(annotationCreated.content);
+                    }
+                    position += word.length+1; // the plus 1 accounts for the space at the end
+                })
+                renderTextareaHighlights(); 
+                }    
+        } else if ($("#search-type").children("option:selected").val() === "delete") {
+            console.log("Searching to delete.");
+            var searching = document.getElementById("search-box").value;
+            if (tagModel.currentCategory === null) {
+                alert('Error: Please select a category first');
+                return;
+            } else if (searching == "" || searching.trim() == ""){
+                alert("Please enter the text you want to highlight");
+                return;
+            } else {
+                var regex = new RegExp("\\b" + searching, 'g');
+                console.log("Searching for " + searching + " in document"); 
+    
+                //Get the contents of the entire document
+                var text = document.getElementById("doc-view").value;
+                // Split text into tokens
+                var token = text.split(/\s/);
+    
+                //Current position in the document
+                var position = 0;
+      
+                token.forEach( function(word){
+                    if(word.match(regex)){
+                        console.log("Found " + word + "\nStart postion: " + position + "\nEnd Position: " + (position + word.length));
+                        tagModel.removeAnnotation(position)
+                    }
+                    position += word.length+1; // the plus 1 accounts for the space at the end
+                })
+                renderTextareaHighlights();
+            }
         }
     } 
 });
