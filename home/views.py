9d980b13147aa5a67e9c3c41cd9ca677058df7b8
Nico <NicoG96@gmail.com>
2019-11-09 18:05:23 -0500

    Add methods to return new login/registration views

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -7,3 +9,20 @@ def home(request):
 
 def tag(request):
     return render(request, 'tag.html')
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("tag")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+
+def login(request):
+    if request.method == "POST":
+        return redirect("tag")
+    return render(request, "registration/login.html")
