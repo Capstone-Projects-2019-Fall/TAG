16cf9227d889ddbeb74aa4ba0e6a7309c44cca04
Nico <NicoG96@gmail.com>
2019-11-09 18:04:17 -0500

    Update URLs

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
-
+from django.urls import path, include
+from django.contrib import admin
 from . import views
 
+
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.home, name='home'),
     path('tag', views.tag, name='tag'),
+    path('mldata', include('mldata.urls')),
 ]
