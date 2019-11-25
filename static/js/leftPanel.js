ac1b60ce28a63a84c3aa32793f2409c3cd9df8ca
Nico <NicoG96@gmail.com>
2019-11-25 13:40:30 -0500

    Add nested annotate menu

--- static/js/leftPanel.js
+++ static/js/leftPanel.js
@@ -1,26 +1,54 @@
 // onload hide all
-$('#dlContainer').hide();
+var dlContainer = $('#dlContainer');
+var annotateContainer = $('#annotateContainer');
+
+dlContainer.hide();
+annotateContainer.hide();
 
 // hovered over download button
 $('#downloadBtn').on('mouseenter', function () {
-    $('#dlContainer').slideDown(300);
+    dlContainer.slideDown(300);
 }).on('mouseleave', function (e) {
     // mouse left button
     // check if mouse hovered over download container
     let goingTo = e.relatedTarget || e.fromElement;
-    if (!$('#dlContainer').has(goingTo).length) {
+    if (!dlContainer.has(goingTo).length) {
         // not container // hide
-        $('#dlContainer').slideUp(300);
-    };
+        dlContainer.slideUp(300);
+    }
 });
 
 // left download container
-$('#dlContainer').on('mouseleave', function (e) {
+dlContainer.on('mouseleave', function (e) {
     // mouse left container
     // check if mouse hovered download button
     let goingTo = e.relatedTarget || e.fromElement;
     if (!$(goingTo).is('#downloadBtn')) {
         // not button // hide
         $(this).slideUp(300);
-    };
+    }
+});
+
+
+$('#annotateBtn').on('mouseenter', function () {
+    annotateContainer.slideDown(300);
+}).on('mouseleave', function (e) {
+    // mouse left button
+    // check if mouse hovered over download container
+    let goingTo = e.relatedTarget || e.fromElement;
+    if (!annotateContainer.has(goingTo).length) {
+        // not container // hide
+        annotateContainer.slideUp(300);
+    }
+});
+
+// left download container
+annotateContainer.on('mouseleave', function (e) {
+    // mouse left container
+    // check if mouse hovered download button
+    let goingTo = e.relatedTarget || e.fromElement;
+    if (!$(goingTo).is('#downloadBtn')) {
+        // not button // hide
+        $(this).slideUp(300);
+    }
 });
