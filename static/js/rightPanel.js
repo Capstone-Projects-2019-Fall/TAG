27307b5fde9771f8b125b126ddbbe98a0c1aed7b
keithudock <keithudock@gmail.com>
2019-12-09 08:25:52 -0500

    Revert "Merge pull request #51 from cis-cs-capstone-course/staging"
    This reverts commit 50dffd4c8b7b91b0d040f97597848c7e8e9d5fca.

--- static/js/rightPanel.js
+++ static/js/rightPanel.js
@@ -1,28 +1,14 @@
-$('#rightArrow').hide();
 var timer;
-
-$('#rightContainer').on('click', function (e) {
-    // check if clicked annotation header
-    if (!$(e.target).is('.annoHeader')) {
-        $(this).css('right', 0);
-        clearTimeout(timer);
-    }
-})
 // mouse is hovered over right area
-.on('mouseenter', function () {
-    $('#rightArrow').fadeIn(100);
+$('#rightContainer').on('mouseenter', function () {
     var that = this;
     // mouse hovered over for 500ms
     timer = setTimeout(function () {
         $(that).css('right', 0);
         clearTimeout(timer);
-        $('#rightArrow').addClass('noDisplay');
     }, 500);
-})
-// mouse left before time elapsed
-.on('mouseleave', function () {
+}).on('mouseleave', function () {
     // mouse left right area
-    $('#rightArrow').fadeOut(100);
     // reset timer
     clearTimeout(timer);
 });
@@ -33,11 +19,6 @@ $(document).on('click', function (e) {
     if (!$(e.target).is('#rightContainer') && !$('#rightContainer').has(e.target).length && !$('#delete-menu').has(e.target).length) {
         // hide
         $('#rightContainer').css('right', '-28%');
-        $('#rightArrow').removeClass('noDisplay');
-        //hide console
-        $('#consoleContainer').css('height', ' 1vh');
-        consoleShown = false;
-        $('#consoleArrow').addClass('upsideDown');
     }
 });
 
@@ -45,21 +26,17 @@ $(document).on('click', function (e) {
 $('#anno-list').on('click', '.annoHeader', function () {
     $(this).next('ul').slideToggle(200);
     $(this).children('.dropArrow').toggleClass('upsideDown');
-
+    
     // aesthetics 
     // is closed // open
     if ($(this).attr('value').charAt(0) === '-') {
-        $(this).attr('value', $(this).attr('value').slice(1));
-        hiddenAnno_list.splice(hiddenAnno_list.indexOf($(this).next('ul').attr('value')), 1);
+        $(this).attr('value', $(this).attr('value', ).slice(1));
         $(this).css('background-color', '');
         $(this).css('color', 'white');
         $(this).children('.dropArrow').css('content', 'url("/static/images/arrowDownWhite.png")');
     } 
     // is opened // close
     else {
-        if (hiddenAnno_list.indexOf($(this).next('ul').attr('value')) === -1) {
-            hiddenAnno_list.push($(this).next('ul').attr('value'));
-        }
         $(this).css('background-color', $(this).attr('value'));
         $(this).attr('value', '-' + $(this).attr('value'));
         $(this).css('color', 'black');
@@ -84,7 +61,7 @@ $('#anno-list').on('contextmenu', '.annotation', function (e) {
 })
 
 // right clicked most recent annotation // bring up delete prompt
-$('#recent').on('contextmenu', function (e) {
+$('#recent').on('contextmenu', function(e) {
     e.preventDefault();
     delete_menu.append(
         $('<li/>', {
